8a900f8b66241eb5e7a9b4fd8d21b4a94465eb2d
Roger Collins <zulrang@gmail.com>
2019-10-02 23:39:38 -0400

    Updated bash template

--- bash.sh
+++ bash.sh
@@ -9,6 +9,8 @@
 # - Always quote variables "$var"
 # - Only use "$@" to pass args through
 # - Pass user supplied args after -- to avoid user passing -x flags (ie. curl -v -- "$@")
+# - Use $(cmd) instead of `cmd` as it doesn't have nesting issues
+# - Use -z (empty) or -n (not empty) instead of testing against other strings
 
 # set flags here so that "bash <script>" doesn't break
 set -euf -o pipefail
