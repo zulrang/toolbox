f6e87af844131489002e207527c3f86b2f5d3ac4
Roger Collins <zulrang@gmail.com>
2019-10-02 23:33:00 -0400

    Updated bash template

--- bash.sh
+++ bash.sh
@@ -1,8 +1,26 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+# Generic base script to use as a template
+#
+# Author: Roger Collins
+#
+# Some caveats:
+# - Try to use Python first
+# - Always quote variables "$var"
+# - Only use "$@" to pass args through
+# - Pass user supplied args after -- to avoid user passing -x flags (ie. curl -v -- "$@")
 
-set -euf -o pipefile
+# set flags here so that "bash <script>" doesn't break
+set -euf -o pipefail
 
-# try to use Python first
-# always quote variables "$var"
-# only use "$@" to pass args through
-# pass user supplied args after -- to avoid user passing -x flags
+#---------------------------------#
+# Function to print error messages
+#
+# Arguments:
+#   Message to print to STDERR
+# Returns:
+#   None
+#---------------------------------#
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
+}
